44576b3bfe052e06a802137671c78ad5c19a1dba
Eduardo HS <eduardohs29@gmail.com>
2022-09-23 20:19:44 -0300

    Estrutura base

---
+++ Reciclo/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+//                new SingIn().setVisible(true);
+            }
+        });
+    }
+}
